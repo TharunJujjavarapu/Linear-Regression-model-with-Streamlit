310f5b30a70e3d9fbfd9d3582db5b399e2207d6b
TharunJujjavarapu <152680466+TharunJujjavarapu@users.noreply.github.com>
2024-02-28 10:12:19 +0530

    Add files via upload

---
+++ streamlit1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Streamlit')
+st.write("hello im creating my first app")
+st.write('Im checking')
+st.write('working or not')
+st.text_input('name','enter my name')
+st.button('click')
